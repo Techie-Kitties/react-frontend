72905fa22d3107c836cdf0b55c4c1ba77a013310
khadishaclarke <khadishaclarke.n@gmail.com>
2025-02-19 20:15:25 -0400

    setting up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { Tour } from "./components/ThreeJS/tour";
+//import { Tour } from "./components/ThreeJS/tour";
 import { Nav } from "./components/Widgets/nav";
 import { Login } from "./components/Pages/login";
 import { Example } from "./components/ReactStarter/example";
@@ -9,13 +9,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 export default function App() {
   return (
-    <Example></Example>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/tour" element={<Tour />} />
-    //     <Route path="/login" element={<Login />} />
-    //   </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
