d1dcdcc504aa2bcd778158d06777358934d00919
JCabral22 <Joshcabral@hotmail.com>
2025-02-20 00:40:29 -0400

    Remove tour import until tour is finalized

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
-import { Tour } from "./components/ThreeJS/tour";
 import { Nav } from "./components/Widgets/nav";
 import { Login } from "./components/Pages/login";
 import { Example } from "./components/ReactStarter/example";
