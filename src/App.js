35a82bdc2227c7494a2242a3ce85355165250883
JCabral22 <Joshcabral@hotmail.com>
2025-02-25 08:02:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import { Nav } from "./components/Widgets/nav";
 import { Login } from "./components/Pages/login";
