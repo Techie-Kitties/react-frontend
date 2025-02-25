8716a46f92e7a382f7305d8a9f68dfee1c53ab18
JCabral22 <Joshcabral@hotmail.com>
2025-02-25 07:54:43 -0400

    add oauth implementation to frontend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+<<<<<<< HEAD
 import { Nav } from "./components/Widgets/nav";
 import { Login } from "./components/Pages/login";
 import { Example } from "./components/ReactStarter/example";
+=======
+import { createRoot } from "react-dom/client";
+
+>>>>>>> b3f9d7bdf228a9bb793e40c23d16214fa8eaa2d3
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Tour } from "./components/ThreeJS/tour";
 
 import React from "react";
+import { Nav } from "./components/Widgets/nav";
+import { Login } from "./components/Pages/login";
+import { Example } from "./components/ReactStarter/example";
 import { Home } from "./components/Pages/home";
 import { About } from "./components/Pages/about";
 import { Eulogy } from "./components/Pages/eulogy";
 import { NotFound } from "./components/Pages/notfound";
+<<<<<<< HEAD
 import { OAuthSuccess } from "./components/Widgets/oauthSuccess";
+=======
+import { Packages } from "./components/Pages/packages";
+//import { Tour } from "./components/ThreeJS/tour";
+
+>>>>>>> b3f9d7bdf228a9bb793e40c23d16214fa8eaa2d3
 export default function App() {
   return (
     <BrowserRouter>
@@ -23,6 +37,7 @@ export default function App() {
         <Route path="/auth" element={<OAuthSuccess />} />
         <Route path="/tour" element={<Tour />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/packages" element={<Packages />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Pages/packages.jsx
@@ -0,0 +1,187 @@
+import { Footer } from "../Widgets/footer";
+import { Nav } from "../Widgets/nav";
+
+export function Packages() {
+    return (
+        <div className=" min-h-screen z-10 relative flex flex-col">
+          <Nav />
+          <div className="flex flex-col sm:flex-row bg-[#2F4754] max-w-screen overflow-x-hidden">
+            <div className="hero z-10 sm:h-80 bg-[#2F4754] flex pl-8 sm:pl-32 pt-16 sm:pt-32 md:text-5xl text-white font-bold">
+            <div className="text-right sm:text-left text-lg sm:text-xl md:text-xl ml-85 max-w-3xl px-8 pt-8">
+            “Honoring Legacies with Thoughtful Memorial Packages”
+            Find the perfect memorial package to cherish and celebrate your loved one’s legacy with care and dignity."
+            </div>
+            <img src="/casket.png" style={{ position: "absolute", left: 0, top: "70px", width: "35%" }} />
+
+
+            </div>
+          </div>
+          <div className="bg-[#161E22] md:flex space-y-12 flex-1 md:space-y-10 z-20 justify-evenly md:max-h-[500px] text-white px-4 ">
+            <div className="flex md:-mt-[-2vw] flex-col shadow-xl shadow-black rounded-xl w-full sm:w-[20vw] max-h-[360px] transform transition-transform duration-300 hover:scale-110">
+              <img
+                className="rounded-t-xl pb-12"
+                src="/basic-img.png"
+              />
+              <div className="text-center text-xl pb-8 font-bold">Basic</div>
+              <div className="pb-7">
+                <svg
+                  className="mx-auto"
+                  width="38"
+                  height="37"
+                  viewBox="0 0 38 37"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M37.5 18.5C37.5 28.4516 29.612 36.5 19.9048 36.5C10.1976 36.5 2.30957 28.4516 2.30957 18.5C2.30957 8.54839 10.1976 0.5 19.9048 0.5C29.612 0.5 37.5 8.54839 37.5 18.5Z"
+                    fill="#DD6E42"
+                    stroke="#DD6E42"
+                  />
+                  <g opacity="0.7">
+                    <rect
+                      x="0.5"
+                      y="0.5"
+                      width="37"
+                      height="35.0263"
+                      stroke="black"
+                      stroke-opacity="0.3"
+                    />
+                    <path
+                      d="M7.91675 18.0132H30.0834M30.0834 18.0132L19.0001 7.50549M30.0834 18.0132L19.0001 28.5208"
+                      stroke="#F3F3F3"
+                      stroke-width="4"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <div className="flex md:-mt-[-2vw] flex-col shadow-xl shadow-black rounded-xl w-full sm:w-[20vw] max-h-[360px] transform transition-transform duration-300 hover:scale-110">
+              <img
+                className="brightness-90 rounded-t-xl pb-12"
+                src="/standard-img.png"
+              />
+              <div className="text-center text-xl pb-8 font-bold">Standard</div>
+              <div className="pb-7">
+                <svg
+                  className="mx-auto"
+                  width="38"
+                  height="37"
+                  viewBox="0 0 38 37"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M37.5 18.5C37.5 28.4516 29.612 36.5 19.9048 36.5C10.1976 36.5 2.30957 28.4516 2.30957 18.5C2.30957 8.54839 10.1976 0.5 19.9048 0.5C29.612 0.5 37.5 8.54839 37.5 18.5Z"
+                    fill="#DD6E42"
+                    stroke="#DD6E42"
+                  />
+                  <g opacity="0.7">
+                    <rect
+                      x="0.5"
+                      y="0.5"
+                      width="37"
+                      height="35.0263"
+                      stroke="black"
+                      stroke-opacity="0.3"
+                    />
+                    <path
+                      d="M7.91675 18.0132H30.0834M30.0834 18.0132L19.0001 7.50549M30.0834 18.0132L19.0001 28.5208"
+                      stroke="#F3F3F3"
+                      stroke-width="4"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <div className="flex md:-mt-[-2vw] flex-col shadow-xl shadow-black rounded-xl w-full sm:w-[20vw] max-h-[360px] transform transition-transform duration-300 hover:scale-110">
+              <img
+                className="brightness-90 rounded-t-xl pb-12"
+                src="/premiuim-img.png"
+              />
+<div className="text-center text-xl pb-8 font-bold">Premium</div>
+              <div className="pb-7">
+                <svg
+                  className="mx-auto"
+                  width="38"
+                  height="37"
+                  viewBox="0 0 38 37"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M37.5 18.5C37.5 28.4516 29.612 36.5 19.9048 36.5C10.1976 36.5 2.30957 28.4516 2.30957 18.5C2.30957 8.54839 10.1976 0.5 19.9048 0.5C29.612 0.5 37.5 8.54839 37.5 18.5Z"
+                    fill="#DD6E42"
+                    stroke="#DD6E42"
+                  />
+                  <g opacity="0.7">
+                    <rect
+                      x="0.5"
+                      y="0.5"
+                      width="37"
+                      height="35.0263"
+                      stroke="black"
+                      stroke-opacity="0.3"
+                    />
+                    <path
+                      d="M7.91675 18.0132H30.0834M30.0834 18.0132L19.0001 7.50549M30.0834 18.0132L19.0001 28.5208"
+                      stroke="#F3F3F3"
+                      stroke-width="4"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <div className="flex md:-mt-[-2vw] flex-col shadow-xl shadow-black rounded-xl w-full sm:w-[20vw] max-h-[360px] transform transition-transform duration-300 hover:scale-110">
+              <img
+                className="brightness-100 rounded-t-xl pb-8"
+                src="/custom_img.png"
+              />
+
+              <div className="text-center text-xl pb-8 font-bold">Customize Your Own</div>
+              <div className="pb-7">
+                <svg
+                  className="mx-auto"
+                  width="38"
+                  height="37"
+                  viewBox="0 0 38 37"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M37.5 18.5C37.5 28.4516 29.612 36.5 19.9048 36.5C10.1976 36.5 2.30957 28.4516 2.30957 18.5C2.30957 8.54839 10.1976 0.5 19.9048 0.5C29.612 0.5 37.5 8.54839 37.5 18.5Z"
+                    fill="#DD6E42"
+                    stroke="#DD6E42"
+                  />
+                  <g opacity="0.7">
+                    <rect
+                      x="0.5"
+                      y="0.5"
+                      width="37"
+                      height="35.0263"
+                      stroke="black"
+                      stroke-opacity="0.3"
+                    />
+                    <path
+                      d="M7.91675 18.0132H30.0834M30.0834 18.0132L19.0001 7.50549M30.0834 18.0132L19.0001 28.5208"
+                      stroke="#F3F3F3"
+                      stroke-width="4"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </g>
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#161E22] flex-1"></div>
+        </div>
+
+        
+      );
+}
