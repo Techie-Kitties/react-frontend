eaedd3fcd6eaeacbb1cbb6f025ccd37bca69519b
JCabral22 <Joshcabral@hotmail.com>
2025-03-25 07:07:05 -0400

    Fix error in App.js

--- src/App.js
+++ src/App.js
@@ -31,30 +31,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
-
-import { Nav } from "./components/Widgets/nav";
-import { Login } from "./components/Pages/login";
-import { Example } from "./components/ReactStarter/example";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import React from "react";
-import { Home } from "./components/Pages/home";
-import { About } from "./components/Pages/about";
-import { Eulogy } from "./components/Pages/eulogy";
-import { NotFound } from "./components/Pages/notfound";
-import { Contact } from "./components/Pages/contact";
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/eulogy" element={<Eulogy />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
